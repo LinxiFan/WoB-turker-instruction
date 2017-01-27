edf71952c8e671e30e81bbde8396136bfc40cce0
Tim Shi <stl501@gmail.com>
2017-01-26 17:28:32 -0800

    Add minimal backend

---
+++ server.py
@@ -0,0 +1,51 @@
+from tornado import (ioloop, web)
+import hashlib
+import string
+import random
+import os
+import json
+
+def wrap_template(template):
+    class TemplateHandler(web.RequestHandler):
+        def get(self):
+            self.render(template)
+    return TemplateHandler
+
+
+db = open('data.jsonl', 'a')
+
+
+class SubmitHandler(web.RequestHandler):
+    def post(self):
+        data = json.loads(self.get_argument('data'))
+        url = self.get_argument('url')
+        question = json.loads(self.get_argument('question'))
+
+        db.write(json.dumps({
+            'data': data,
+            'url': url,
+            'question': question
+        }) + '\n')
+        db.flush()
+        self.write('OK')
+
+
+handlers = [
+    (r"/", wrap_template('index.html')),
+    (r"/submit", SubmitHandler),
+    (r"/(.*\.js)", web.StaticFileHandler, {"path": "./"}),
+    (r"/(.*\.css)", web.StaticFileHandler, {"path": "./"}),
+]
+
+settings = {
+    "autoreload": True,
+    "debug": True,
+    "template_path": "./",
+}
+
+if __name__ == "__main__":
+    # start main application.
+    application = web.Application(handlers, **settings)
+    port = int(os.environ.get("PORT", 8080))
+    application.listen(port, address="0.0.0.0")
+    ioloop.IOLoop.current().start()
